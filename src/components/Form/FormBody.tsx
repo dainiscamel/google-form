20afee08c75ba23e7d9d9a497427f4376e6e8512
dainiscamel <juhyueon3@naver.com>
2025-01-09 18:58:46 +0900

    refactor 바텀 툴바 분리

--- src/components/Form/FormBody.tsx
+++ src/components/Form/FormBody.tsx
@@ -1,18 +1,9 @@
 import { useState } from "react";
-import { Radio, IconButton, Switch, SelectChangeEvent } from "@mui/material";
+import { Radio, SelectChangeEvent } from "@mui/material";
 import styled from "styled-components";
 import FormInput from "./Input";
 import FormSelect from "@/components/Form/FormSelect";
-
-import ImageIcon from "@mui/icons-material/Image";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import FormatBoldIcon from "@mui/icons-material/FormatBold";
-import FormatItalicIcon from "@mui/icons-material/FormatItalic";
-import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
-import InsertLinkIcon from "@mui/icons-material/InsertLink";
-import ContentCutIcon from "@mui/icons-material/ContentCut";
+import FormBottomToolbar from "./FormBottomToolbar";
 
 interface Option {
   id: string;
@@ -27,6 +18,10 @@ const FormBody = () => {
     { id: "2", text: "옵션 추가 또는 '기타' 추가" },
   ]);
   const [required, setRequired] = useState(false);
+
+  const handleRequiredChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRequired(e.target.checked);
+  };
 
   const handleTypeChange = (e: SelectChangeEvent<string>) => {
     setQuestionType(e.target.value);
@@ -66,30 +61,9 @@ const FormBody = () => {
           }}
         />
         <RightSection>
-          <ImageButton>
-            <ImageIcon />
-          </ImageButton>
           <Select value={questionType} onChange={handleTypeChange} />
         </RightSection>
       </QuestionHeader>
-
-      <FormatToolbar>
-        <IconButton size="small">
-          <FormatBoldIcon />
-        </IconButton>
-        <IconButton size="small">
-          <FormatItalicIcon />
-        </IconButton>
-        <IconButton size="small">
-          <FormatUnderlinedIcon />
-        </IconButton>
-        <IconButton size="small">
-          <InsertLinkIcon />
-        </IconButton>
-        <IconButton size="small">
-          <ContentCutIcon />
-        </IconButton>
-      </FormatToolbar>
 
       <OptionsContainer>
         {options.map((option, index) => (
@@ -114,27 +88,10 @@ const FormBody = () => {
         ))}
       </OptionsContainer>
 
-      <BottomToolbar>
-        <ToolbarLeft>
-          <IconButton>
-            <ContentCopyIcon />
-          </IconButton>
-          <IconButton>
-            <DeleteOutlineIcon />
-          </IconButton>
-        </ToolbarLeft>
-        <ToolbarRight>
-          <RequiredText>필수</RequiredText>
-          <Switch
-            size="small"
-            checked={required}
-            onChange={(e) => setRequired(e.target.checked)}
-          />
-          <IconButton>
-            <MoreVertIcon />
-          </IconButton>
-        </ToolbarRight>
-      </BottomToolbar>
+      <FormBottomToolbar
+        required={required}
+        onRequiredChange={handleRequiredChange}
+      />
     </Container>
   );
 };
@@ -171,10 +128,6 @@ const RightSection = styled.div`
   gap: 8px;
 `;
 
-const ImageButton = styled(IconButton)`
-  color: rgba(0, 0, 0, 0.54);
-`;
-
 const Select = styled(FormSelect)`
   min-width: 200px;
   height: 40px;
@@ -190,14 +143,6 @@ const Select = styled(FormSelect)`
   }
 `;
 
-const FormatToolbar = styled.div`
-  display: flex;
-  gap: 4px;
-  padding: 8px 0;
-  border-bottom: 1px solid rgb(218, 220, 224);
-  margin: 16px 0;
-`;
-
 const OptionsContainer = styled.div`
   margin: 16px 0;
 `;
@@ -206,31 +151,6 @@ const OptionRow = styled.div`
   display: flex;
   align-items: center;
   margin: 8px 0;
-`;
-
-const BottomToolbar = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 16px;
-  padding-top: 16px;
-  border-top: 1px solid rgb(218, 220, 224);
-`;
-
-const ToolbarLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const ToolbarRight = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const RequiredText = styled.span`
-  color: rgba(0, 0, 0, 0.6);
-  font-size: 14px;
-  margin-right: 8px;
 `;
 
 const QuestionInput = styled(FormInput)`
---
+++ src/components/Form/FormBottomToolbar.tsx
@@ -0,0 +1,63 @@
+// components/Form/FormBottomToolbar.tsx
+import { IconButton, Switch } from "@mui/material";
+import styled from "styled-components";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+interface FormBottomToolbarProps {
+  required: boolean;
+  onRequiredChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const FormBottomToolbar = ({
+  required,
+  onRequiredChange,
+}: FormBottomToolbarProps) => {
+  return (
+    <Toolbar>
+      <ToolbarLeft>
+        <IconButton>
+          <ContentCopyIcon />
+        </IconButton>
+        <IconButton>
+          <DeleteOutlineIcon />
+        </IconButton>
+      </ToolbarLeft>
+      <ToolbarRight>
+        <RequiredText>필수</RequiredText>
+        <Switch size="small" checked={required} onChange={onRequiredChange} />
+        <IconButton>
+          <MoreVertIcon />
+        </IconButton>
+      </ToolbarRight>
+    </Toolbar>
+  );
+};
+
+const Toolbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 16px;
+  padding-top: 16px;
+  border-top: 1px solid rgb(218, 220, 224);
+`;
+
+const ToolbarLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ToolbarRight = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const RequiredText = styled.span`
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 14px;
+  margin-right: 8px;
+`;
+
+export default FormBottomToolbar;
